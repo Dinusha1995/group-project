04a246015b9514718b14697a28b9276355b2b1e4
Indika Weerasundara <prasanna.weer@gmail.com>
2026-06-08 22:53:15 +0530

    add student-feature-model

---
+++ Indika.java
@@ -0,0 +1,80 @@
+class Indika {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String email;
+    private String grade;
+
+    public Indika() {
+    }
+
+    public Indika(int id, String firstName, String lastName, int age, String email, String grade) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+        this.grade = grade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Indika{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                ", grade='" + grade + '\'' +
+                '}';
+    }
+}
